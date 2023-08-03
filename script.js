4f2f9bb61554a622845f6d24cfa06aba5dcdba6e
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-08-03 09:38:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-const app = require("./index.js");
-app.listen(3000, () => {
-  console.log('server started');
-});
+function majorityElementChecker(arr)
+    {
+        let map = new Map();
+ 
+        for(let i = 0; i < arr.length; i++) {
+            if (map.has(arr[i])) {
+                    let count = map.get(arr[i]) +1;
+                    if (count > arr.length /2) {
+                      //  document.write("Majority found :- " + arr[i]);
+                        return arr[i];
+                    } else
+                        map.set(arr[i], count);
+ 
+            }
+            else
+                map.set(arr[i],1);
+            }
+            return 1;
+            //document.write(" No Majority element");
+    }
+
+module.exports = majorityElementChecker;
